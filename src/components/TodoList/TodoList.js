fd8b10fa095cdbdf24195a30a33d3b6b145920b9
ivanov <ivanov@WSR-PREP>
2022-04-11 11:47:15 +0300

    Передаём свойства в виде объекта. ОШИБКА. Первый компонент из спика так и осталась передача свойств в виде "атрибутов".

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,6 +3,47 @@ import TodoItem from "./TodoItem/TodoItem";
 
 
 function TodoList() {
+    let todoList =
+        [
+            {
+                name: "ES6 import",
+                datetime: "вчера",
+                text: "Необходимо выучить и разобрать отличия работы с модулями в JS и NodeJS.",
+                status: "Статус: Срочно",
+            },
+            {
+                name: "Завтрак",
+                datetime: "2 дня назад",
+                text: "Приготовить любимой завтрак в постель.",
+                status: "Статус: главное не забыть",
+            },
+            {
+                name: "React",
+                datetime: "6 дней назад",
+                text: "Изучить основы React. Возможности ES2016.",
+                status: "Срок: 7 дней на выполнение",
+            },
+            {
+                name: "ДемоЭкзамен",
+                datetime: "месяц назад",
+                text: "Организация подготовки студентов к демонстрационному экзамену по стандартам WorldSkills",
+                status: "Срок: 12 апреля",
+            },
+            {
+                name: "Проверка д/з",
+                datetime: "неделю назад",
+                text: "Проверка домашнего задания 8-ой недели подготовки студентов к экзамену.",
+                status: "Выполнено",
+            },
+            {
+                name: "Разработка todo-приложения",
+                datetime: "сегодня",
+                text: "Создать на основе примера из книге по React приложение для списка дел.",
+                status: "Срок: неопределенно",
+            },
+        ];
+
+    console.log(todoList);
 
     return (
         <ul className="list-group">
@@ -12,9 +53,9 @@ function TodoList() {
                 text="Создать на основе примера из книге по React приложение для списка дел."
                 status="Срок: неопределенно"
             />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            <TodoItem data={todoList[1]} />
+            <TodoItem data={todoList[2]} />
+            <TodoItem data={todoList[3]}/>
         </ul>
     );
 }
