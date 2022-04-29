d4e70b87f8c68c92c805b4026cf3b8c57ad9914c
Ilya Ivanov <zamt.it@yandex.ru>
2022-04-29 23:22:30 +0300

    Компонент todoItem меняет стиль в зависимости от статуса todo и просрочки по выполнению.

--- src/components/TodoList/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItem/TodoItem.js
@@ -3,8 +3,26 @@ import React from "react";
 
 function TodoItem(props) {
 
+    let listGroupStyle;
+    let tickNow = (new Date()).getTime();
+    let previousDay = Math.ceil((tickNow - props.datetime) / (1000*3600*24));
+
+
+    switch (props.status) {
+        case "error":
+            listGroupStyle = "list-group-item-danger";
+            break;
+
+        case "success":
+            listGroupStyle = "list-group-item-success";
+            break;
+
+        default:
+            listGroupStyle = previousDay >= 30 ? "list-group-item-warning" : "";
+    }
+
     return (
-        <li className="list-group-item list-group-item-action">
+        <li className={`list-group-item list-group-item-action ${listGroupStyle}`}>
             <div className="d-flex w-100 justify-content-between">
                 <h5 className="mb-1">{props.title}</h5>
                 <small className="text-muted">{props.datetime}</small>
