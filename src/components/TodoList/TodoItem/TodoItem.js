7af3a6eaa905899af60de1be7dd1d98b55fd03d3
Ilya Ivanov <zamt.it@yandex.ru>
2022-04-30 22:38:05 +0300

    Компонент todoItem. Выводится дата выдачи задачи. Использовалось форматирование через параметр опций.

--- src/components/TodoList/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItem/TodoItem.js
@@ -4,9 +4,12 @@ import React from "react";
 function TodoItem(props) {
 
     let listGroupStyle;
+    // число тиков с начало эпохи минус время из свойства компонента
     let tickNow = (new Date()).getTime();
-    let previousDay = Math.ceil((tickNow - props.datetime) / (1000*3600*24));
-
+    let previousDay = Math.ceil((tickNow - props.datetime) / (1000*3600*24)); // количество дней
+    // дата выдачи
+    let dateOfIssue = new Date(props.datetime);
+    let options = {day: "2-digit", month:"2-digit", year: "numeric" };
 
     switch (props.status) {
         case "error":
@@ -25,7 +28,7 @@ function TodoItem(props) {
         <li className={`list-group-item list-group-item-action ${listGroupStyle}`}>
             <div className="d-flex w-100 justify-content-between">
                 <h5 className="mb-1">{props.title}</h5>
-                <small className="text-muted">{props.datetime}</small>
+                <small className="text-muted">{dateOfIssue.toLocaleDateString("ru", options)}</small>
             </div>
             <p className="mb-1">{props.description}</p>
             <small className="text-muted">{props.note}</small>
