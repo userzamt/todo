7b4b3067cd095f2ee0cc7cbe594d4d143692d593
Ilya Ivanov <zamt.it@yandex.ru>
2022-05-08 16:21:06 +0300

    constructor(props) + {деструктуризация, объекта} Преобразовал TodoItem в компонент на классах. Добавил в состояние информацию о todo переданую через свойства от TodoList. Для доступа к свойствам в конструкторе - изменел его на constructor(props) и добавил строчку с передачей свойств родительскому объекту super(props). Так же в конструктуре воспользовался деструктуризацией объекта для разделения свойств объекта на переменные.

--- src/components/TodoList/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItem/TodoItem.js
@@ -2,47 +2,55 @@ import React from "react";
 import "./TodoItem.css";
 
 
-function TodoItem(props) {
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
 
-    let listGroupStyle;
-    // число тиков с начало эпохи минус время из свойства компонента
-    let tickNow = (new Date()).getTime();
-    let previousDay = Math.ceil((tickNow - props.datetime) / (1000*3600*24)); // количество дней
-    // дата выдачи
-    let dateOfIssue = new Date(props.datetime);
-    let options = {day: "2-digit", month:"2-digit", year: "numeric" };
-
-    switch (props.status) {
-        case "error":
-            listGroupStyle = "list-group-item-danger";
-            break;
-
-        case "success":
-            listGroupStyle = "list-group-item-success";
-            break;
-
-        default:
-            listGroupStyle = previousDay >= 30 ? "list-group-item-warning" : "";
+        let {title, datetime, description, note, status} = props;
+        this.state = {title, datetime, description, note, status};
     }
 
-    return (
-        <li className={`list-group-item list-group-item-action ${listGroupStyle}`}>
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">{props.title}</h5>
-                <small className="text-muted">{dateOfIssue.toLocaleDateString("ru", options)}</small>
-            </div>
-            <div className="d-flex w-100 justify-content-between">
-                <div>
-                    <p className="mb-1 me-4 text-justify">{props.description}</p>
-                    <small className="text-muted">{props.note}</small>
-                </div>
-                <div className="form-check form-switch align-self-lg-center">
-                    <input className="form-check-input" type="checkbox"  checked={props.status === "success" } />
-                </div>
-            </div>
+    render() {
+        let listGroupStyle;
+        // число тиков с начало эпохи минус время из свойства компонента
+        let tickNow = (new Date()).getTime();
+        let previousDay = Math.ceil((tickNow - this.state.datetime) / (1000*3600*24)); // количество дней
+        // дата выдачи
+        let dateOfIssue = new Date(this.state.datetime);
+        let options = {day: "2-digit", month:"2-digit", year: "numeric" };
 
-        </li>
-    );
+        switch (this.state.status) {
+            case "error":
+                listGroupStyle = "list-group-item-danger";
+                break;
+
+            case "success":
+                listGroupStyle = "list-group-item-success";
+                break;
+
+            default:
+                listGroupStyle = previousDay >= 30 ? "list-group-item-warning" : "";
+        }
+
+        return (
+            <li className={`list-group-item list-group-item-action ${listGroupStyle}`}>
+                <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">{this.state.title}</h5>
+                    <small className="text-muted">{dateOfIssue.toLocaleDateString("ru", options)}</small>
+                </div>
+                <div className="d-flex w-100 justify-content-between">
+                    <div>
+                        <p className="mb-1 me-4 text-justify">{this.state.description}</p>
+                        <small className="text-muted">{this.state.note}</small>
+                    </div>
+                    <div className="form-check form-switch align-self-lg-center">
+                        <input className="form-check-input" type="checkbox"  checked={this.state.status === "success" } />
+                    </div>
+                </div>
+
+            </li>
+        );
+    }
 }
 
 export default TodoItem;
