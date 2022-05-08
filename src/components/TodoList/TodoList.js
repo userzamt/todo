a681ccb65efd854dca4cd889196f71dbabd50b06
Ilya Ivanov <zamt.it@yandex.ru>
2022-05-08 15:44:14 +0300

    constructor(props)!!! Преобразовал TodoList в компонент на классах. Добавил в состояние список дел переданый через свойства от App. Для доступа к свойствам в конструкторе - изменел его на constructor(props) и добавил строчку с передачей свойств родительскому объекту super(props).

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,22 +2,34 @@ import React from "react";
 import TodoItem from "./TodoItem/TodoItem";
 
 
-function TodoList(props) {
+class TodoList extends React.Component{
+    // чтобы иметь доступ к передаваемым свойсвам укзываем props
+    constructor(props) {
+        super(props);
 
-    let todoList= props.data.map(
-        (item, key) => <TodoItem key={key}
-                                 title={item.title}
-                                 datetime={item.datetime}
-                                 description={item.description}
-                                 note={item.note}
-                                 status={item.status}/>
-    );
+        let data = props.data; /* промежуточное звено перед занесением в состояние компонента */
 
-    return (
-        <ul className="list-group">
-            {todoList}
-        </ul>
-    );
+        this.state = {
+            data,
+        };
+    }
+
+    render() {
+        let todoList = this.state.data.map(
+            (item, key) => <TodoItem key={key}
+                                     title={item.title}
+                                     datetime={item.datetime}
+                                     description={item.description}
+                                     note={item.note}
+                                     status={item.status}/>
+        );
+
+        return (
+            <ul className="list-group">
+                {todoList}
+            </ul>
+        );
+    }
 }
 
 export default TodoList;
